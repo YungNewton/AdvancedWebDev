cc3a47783d7c6e8c66f4b274d6859e2719ee198f
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-01 19:30:05 +0100

    commit

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ function change(){
     paragraph.textContent = "Scroll down to use our love calc.";
 }
 document.querySelector(".Try_out").onclick = function(){change()};
-var input1 = document.querySelector(".input").value
-var input2 = document.querySelector(".input2").value
+var input1 = document.querySelector(".input").value;
+var input2 = document.querySelector(".input2").value;
 var combinationOfInput = input1 + input2;
-var TrueCounter = 0;
-var LoveCounter = 0;
+combinationOfInput = combinationOfInput.toLowerCase();
+var trueCounter = 0;
+var loveCounter = 0;
 for (var i = 0; i < combinationOfInput.length; i++){
-    
+    if(combinationOfInput.charAt(i) === "t" || combinationOfInput.charAt(i) === "r"){
+        trueCounter++;
+    }else if(combinationOfInput.charAt(i) === "u" || combinationOfInput.charAt(i) === "e"){
+        trueCounter++;
+    }
+    if(combinationOfInput.charAt(i) === "l" || combinationOfInput.charAt(i) === "o"){
+        loveCounter++;
+    }else if(combinationOfInput.charAt(i) === "v" || combinationOfInput.charAt(i) === "e"){
+        loveCounter++;
+    }
 }
+trueCounter = trueCounter * 10;
+var percentage = trueCounter + loveCounter;
 function TheAlert(){
-    
+    document.querySelector(".value").textContent = combinationOfInput;
 }
 document.querySelector(".submit").onclick = function(){TheAlert()};
